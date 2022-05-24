7e71c3633fdcc43f4ded9cbbb86704da03b0d1e3
GiselleLeadebal <giselleleadebal@gmail.com>
2022-05-24 20:59:05 -0300

    subindo estilização

---
+++ js/controllers/botoes-controller.js
@@ -0,0 +1,22 @@
+//BOTAO HEADER
+
+//BOTAO PÁGINA INICIAL
+
+
+// BOTÕES DOS PLANOS 
+
+document.getElementById("planosbasico").onclick = function () {
+    window.location = "./html/cadastro.html";
+};
+
+document.getElementById("planosjunior").onclick = function () {
+    window.location = "./html/cadastro.html";
+};
+
+document.getElementById("planospleno").onclick = function () {
+    window.location = "./html/cadastro.html";
+};
+
+document.getElementById("planossenior").onclick = function () {
+    window.location = "./html/cadastro.html";
+};
