82427d53c8fea2547340b28e0d269eabff5ff228
andre-sqr <andrebsqr@gmail.com>
2022-05-25 23:23:36 -0300

    links do index consertados

--- js/controllers/botoes-controller.js
+++ js/controllers/botoes-controller.js
@@ -1,3 +1,13 @@
+// BOTÕES DA HEADER
+
+document.getElementById("assinaragora").onclick = function () {
+    window.location = "./html/cadastro.html";
+};
+
+document.getElementById("btnconfiranossosplanos").onclick = function () {
+    window.location = "././index.html#planos";
+};
+
 
 // BOTÕES DOS PLANOS 
 
